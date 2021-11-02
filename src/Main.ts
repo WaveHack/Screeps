ddb1fead5323737c0ac4f9a5ea088956824db1cc
WaveHack <email@wavehack.net>
2021-11-02 23:54:36 +0100

    Auto spawn up to 2 Harvesters

--- src/Main.ts
+++ src/Main.ts
@@ -11,6 +11,14 @@ export function loop() {
         }
     }
 
+    const harvesters = _.filter(Game.creeps, creep => creep.memory.role === 'harvester');
+
+    if (harvesters.length < 2) {
+        const newName = ('Harvester' + Game.time);
+        console.log(`Spawning new Harvester: ${newName}`);
+        Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], newName, {memory: {role: 'harvester'}});
+    }
+
     for (const name in Game.creeps) {
         const creep = Game.creeps[name];
 
