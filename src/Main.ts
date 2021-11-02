4c461b1ea5fb469026b8905c7b702f977fea069c
WaveHack <email@wavehack.net>
2021-11-03 00:53:26 +0100

    Move spawning creeps to SpawnManager.ts

--- src/Main.ts
+++ src/Main.ts
@@ -1,46 +1,10 @@
-import {Harvester} from "./Roles/Harvester";
-import {Upgrader} from "./Roles/Upgrader";
-import {Builder} from "./Roles/Builder";
 import {MemoryManager} from "./Managers/MemoryManager";
 import {CreepManager} from "./Managers/CreepManager";
+import {SpawnManager} from "./Managers/SpawnManager";
 
 //noinspection JSUnusedGlobalSymbols
 export function loop() {
-
     MemoryManager.CleanupInactiveCreeps();
-
-    const spawn = Game.spawns['Spawn1'];
-
-    const harvesters = _.filter(Game.creeps, creep => creep.memory.role === 'harvester');
-    const upgraders = _.filter(Game.creeps, creep => creep.memory.role === 'upgrader');
-    const builders = _.filter(Game.creeps, creep => creep.memory.role === 'builder');
-
-    if (!spawn.spawning) {
-        if (harvesters.length < 2) {
-            const newName = ('Harvester' + Game.time);
-
-            if (spawn.spawnCreep([WORK, CARRY, MOVE], newName,  {memory: {role: 'harvester', task: 'harvest'}}) === OK) {
-                console.log(`Spawning new Harvester: ${newName}`);
-            }
-        }
-
-        if (builders.length < 3) {
-            const newName = ('Builder' + Game.time);
-
-            if (spawn.spawnCreep([WORK, CARRY, MOVE], newName, {memory: {role: 'builder', task: 'harvest'}}) === OK) {
-                console.log(`Spawning new Builder: ${newName}`);
-            }
-        }
-
-        if (upgraders.length < 6) {
-            const newName = ('Upgrader' + Game.time);
-
-            if (spawn.spawnCreep([WORK, CARRY, MOVE], newName, {memory: {role: 'upgrader', task: 'harvest'}}) === OK) {
-                console.log(`Spawning new Upgrader: ${newName}`);
-            }
-        }
-    }
-
+    SpawnManager.SpawnCreeps();
     CreepManager.RunCreeps();
-
 }
---
+++ src/Managers/SpawnManager.ts
@@ -0,0 +1,43 @@
+import {Builder} from "../Roles/Builder";
+import {Harvester} from "../Roles/Harvester";
+import {Upgrader} from "../Roles/Upgrader";
+
+export class SpawnManager {
+
+    static spawn: StructureSpawn = Game.spawns['Spawn1'];
+
+    static SpawnCreeps(): void {
+        if (this.spawn.spawning) {
+            return;
+        }
+
+        const harvesters = _.filter(Game.creeps, creep => creep.memory.role === 'harvester');
+        const upgraders = _.filter(Game.creeps, creep => creep.memory.role === 'upgrader');
+        const builders = _.filter(Game.creeps, creep => creep.memory.role === 'builder');
+
+        if (harvesters.length < 2) {
+            const newName = ('Harvester' + Game.time);
+
+            if (this.spawn.spawnCreep([WORK, CARRY, MOVE], newName,  {memory: {role: 'harvester', task: 'harvest'}}) === OK) {
+                console.log(`Spawning new Harvester: ${newName}`);
+            }
+        }
+
+        if (builders.length < 3) {
+            const newName = ('Builder' + Game.time);
+
+            if (this.spawn.spawnCreep([WORK, CARRY, MOVE], newName, {memory: {role: 'builder', task: 'harvest'}}) === OK) {
+                console.log(`Spawning new Builder: ${newName}`);
+            }
+        }
+
+        if (upgraders.length < 6) {
+            const newName = ('Upgrader' + Game.time);
+
+            if (this.spawn.spawnCreep([WORK, CARRY, MOVE], newName, {memory: {role: 'upgrader', task: 'harvest'}}) === OK) {
+                console.log(`Spawning new Upgrader: ${newName}`);
+            }
+        }
+    }
+
+}
