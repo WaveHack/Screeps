7f12b1b760122b32e41cca6567aff7f3715677cc
WaveHack <email@wavehack.net>
2021-11-02 21:50:13 +0100

    Install and configure Rollup as build script

---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+'use strict';
+
+import clear from 'rollup-plugin-clear';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from 'rollup-plugin-typescript2';
+import screeps from 'rollup-plugin-screeps';
+
+const cfg = require('./screeps.json');
+
+export default {
+    input: 'src/Main.ts',
+
+    output: {
+        file: 'dist/main.js',
+        format: 'cjs',
+        sourcemap: true,
+    },
+
+    plugins: [
+        clear({targets: ['dist']}),
+        resolve({rootDir: 'src'}),
+        commonjs(),
+        typescript({tsconfig: './tsconfig.json'}),
+        screeps({config: cfg, dryRun: true}),
+    ],
+
+}
